06ea7a777eeb577e8adc408af3f97ce2d6066c59
workbhavya302 <work.bhavya302@gmail.com>
2026-08-12 22:18:26 +0530

    Time: 191 ms (55.06%) | Memory: 35.1 MB (66.75%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+class Solution:
+    def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
+        q = deque() 
+        res = []
+        
+        for i in range(len(nums)):
+            while q and nums[q[-1]] <= nums[i]:
+                q.pop()   
+            q.append(i)
+            
+            if q[0] < i - k + 1:
+                q.popleft()
+            if i >= k - 1:
+                res.append(nums[q[0]])
+                
+        return res
+
+        
